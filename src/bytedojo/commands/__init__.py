830f13ab6ba71ca192d62ce503e6c5497733d2c4
stephen-os <ImStephen.1sw@gmail.com>
2025-12-16 21:25:04 -0500

    feat: added test command for running tests on the tracked files in the database

--- src/bytedojo/commands/__init__.py
+++ src/bytedojo/commands/__init__.py
@@ -5,8 +5,9 @@ Commands package for bytedojo/dojo.
 # ByteDojo
 from bytedojo.commands.init import init
 from bytedojo.commands.stats import stats
+from bytedojo.commands.test import test
 
 # LeetCode
 from bytedojo.commands.leetcode import leetcode
 
-__all__ = ['init', 'stats','leetcode']
+__all__ = ['init', 'stats', 'test', 'leetcode']
